05e891fc365b5c1b545c7f6b292a79c85b1c91e9
Nick <nik.silyaev@gmail.com>
2020-08-04 21:21:49 -0700

    Major commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'; 
-import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
@@ -10,10 +9,6 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '2969b0753dd84aaaa8025510d09c85eb'
-});
-
 const particlesOptions = {
   particles: {
     number:{
@@ -23,19 +18,38 @@ const particlesOptions = {
         value_area: 800
       }
     }
+  } 
+};
+
+const initialState = {
+  input:'',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user:{
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
   }
 };
 
 class App extends Component{
   constructor(){
     super();
-    this.state = {
-      input:'',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-    }
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }})
   }
 
   calculateFaceLocation = (data) => {
@@ -61,14 +75,37 @@ class App extends Component{
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    fetch('http://localhost:3000/imageurl',{
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+                input: this.state.input
+          })
+        })
+    .then(response => response.json()) 
+    .then(response => {
+      if(response){
+        fetch('http://localhost:3000/image',{
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+                id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+          .catch(err => console.log(err));
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({isSignedIn: false});
+      this.setState(initialState);
     }else if(route === 'home'){
       this.setState({isSignedIn: true});
     }
@@ -84,15 +121,15 @@ class App extends Component{
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit} />
               <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>
           : ( route === 'signin'
-            ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
         }
       </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return(
         <div>
             <div className='white f3'>
-                {'Nick, your current rank is...'}
-            </div>
-            <div className='white f1'>
-                {'#3'}
+                {`${name} , your current entry count is...`}
+
+                <div className='white f1 '>
+                    {entries}
+                </div>
             </div>
         </div>
     );
